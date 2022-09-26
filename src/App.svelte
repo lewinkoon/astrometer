905e6a5217af572affe32f6160211610fc4963f1
lewinkoon <lewinkoon+github@gmail.com>
2022-09-26 17:33:40 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onMount } from "svelte";
+  import Chart from "chart.js/auto";
+
+  let ctx;
+
+  onMount(async () => {
+    const myChart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: ["January", "February", "March", "April", "May", "June"],
+        datasets: [
+          {
+            label: "Label",
+            data: [0, 10, 5, 2, 20, 30, 45],
+          },
+        ],
+      },
+    });
+  });
+</script>
+
+<canvas id="myChart" width="400" height="100" bind:this={ctx} />
