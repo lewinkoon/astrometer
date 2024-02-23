c1f78c8a447cbdeed6a4f4266fa9f2746c181dfe
Lewin <57636993+lewinkoon@users.noreply.github.com>
2024-02-23 18:20:50 +0100

    add default commands

--- functions/index.ts
+++ functions/index.ts
@@ -3,17 +3,31 @@ import {
   webhookCallback,
 } from "https://deno.land/x/grammy@v1.21.1/mod.ts";
 import { toIMF } from "https://deno.land/std@0.160.0/datetime/mod.ts";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.39.7";
+
+const supabase = createClient(
+  Deno.env.get("SUPABASE_URL") ?? "",
+  Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+);
 
 // get bot token
 const token = Deno.env.get("BOT_TOKEN");
 if (!token) throw new Error("BOT_TOKEN is unset");
 const bot = new Bot(token);
 
-// set bot commands
+await bot.api.setMyCommands([
+  { command: "start", description: "Start the bot" },
+  { command: "help", description: "Show help text" },
+  { command: "ping", description: "Check status" },
+]);
+
+// show bot info
 bot.command(
   "start",
-  (ctx) => ctx.reply("Hi there! This is Astrobot."),
+  (ctx) => ctx.reply("Hi there! This is Astrobot 🛰️"),
 );
+
+// check bot status
 bot.command(
   "ping",
   (ctx) => ctx.reply(`Pong! ${toIMF(new Date())}`),
