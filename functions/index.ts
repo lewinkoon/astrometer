b16609d3316e0ee1da937b4b6f3088d90eea6e22
Lewin <57636993+lewinkoon@users.noreply.github.com>
2024-02-23 17:13:59 +0100

    setup telegram bot

--- functions/index.ts
+++ functions/index.ts
@@ -1,1 +1,34 @@
-Deno.serve((req: Request) => new Response("Hello World"));
+import {
+  Bot,
+  webhookCallback,
+} from "https://deno.land/x/grammy@v1.21.1/mod.ts";
+
+// get bot token
+const token = Deno.env.get("BOT_TOKEN");
+if (!token) throw new Error("BOT_TOKEN is unset");
+const bot = new Bot(token);
+
+// set bot commands
+bot.command(
+  "start",
+  (ctx) => ctx.reply("Hi there! This is Astrobot. How can i help you?"),
+);
+bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()}`));
+
+// setup webhook
+const handleUpdate = webhookCallback(bot, "std/http");
+
+// init web server
+Deno.serve(async (req) => {
+  if (req.method === "POST") {
+    const url = new URL(req.url);
+    if (url.pathname.slice(1) === bot.token) {
+      try {
+        return await handleUpdate(req);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+  return new Response();
+});
